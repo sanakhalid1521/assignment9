4bd86215e84c28877e09f4b8341385b54fc4836f
Sana Khalid <160645960+sanakhalid1521@users.noreply.github.com>
2024-11-23 16:37:02 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Cards from "./components/cards";
+
+export default function Home() {
+  return (
+   <div>
+<Navbar />
+<Hero />
+<Cards />
+
+
+   </div>
+  );
+}
